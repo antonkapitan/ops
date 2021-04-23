a3542c52d67bbfd1172210cd4446fd8ad7b267b4
antonkapitan <anton.kapitan@bazaarvoice.com>
2021-04-23 16:04:33 +0300

    add mkdir.sh

---
+++ mkdir.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function mk(){
+    local RANDOM_MANTH=$(shuf -i 1-12 -n 1)
+    LENGM=$(expr length "$RANDOM_MANTH")
+    if [[ $LENGM == 2 ]]; then
+        MON="$RANDOM_MANTH"
+    else
+        MON="0$RANDOM_MANTH"
+    fi
+    local RANDOM_DAY=$(shuf -i 1-31 -n 1)
+    LENGD=$(expr length "$RANDOM_DAY")
+    if [[ $LENGD == 2 ]]; then
+        DAY="$RANDOM_DAY"
+    else
+        DAY="0$RANDOM_DAY"
+    fi
+    local DATETIME=$(date '+%H:%M:%S')
+
+    local YEAR=2021
+
+    mkdir $YEAR-$MON-$DAY_$DATETIME
+    touch -mad "$YEAR-$MON-$DAY $DATETIME" $YEAR-$MON-$DAY_$DATETIME
+}
+
+for run in {1..10}
+do
+    mk
+done
